034a68b25ac0b023f51a5a66730b3b6bb5159d69
Ümit Dayangaç <umitdayangac@gmail.com>
2025-07-15 00:40:40 +0300

    DataAccess Layer & Entityl Layer

---
+++ youtube/muratYucedag/MVCProjectCampi-1/DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>,ICategoryDal
+    {
+        
+    }
+}
